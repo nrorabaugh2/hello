3ee1ba63c4d27394290de956aa43b97e3e622ec5
Nick Rorabaugh <nrorabaugh2@huskers.unl.edu>
2024-08-29 09:51:24 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Nick Rorabaugh, Taver Versch
+ * Date: 2024/08/29
+ *
+ * Hack #1
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Nick Rorabaugh\n");
+    
+}
